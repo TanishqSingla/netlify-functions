d74328857ed976d7ca2064205de0f5ef47feb9a1
TanishqSingla <tanishqsingla00@gmail.com>
2024-01-21 17:47:44 +0530

    feat: add cors middleware

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const cors = require("cors");
 
 const app = express();
 const router = express.Router();
 
+app.use(cors());
 router.get("/hello", (req, res) => res.status(200).json({hello: "world"}));
 app.use("/api/", router);
 
