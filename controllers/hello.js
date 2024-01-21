ab916fb38e2a76805f75631fef70f9d5ee80a34d
TanishqSingla <tanishqsingla00@gmail.com>
2024-01-21 20:12:46 +0530

    add env for testing

--- controllers/hello.js
+++ controllers/hello.js
@@ -1,5 +1,5 @@
 function helloController(_req, res) {
-	res.status(200).json({ hello: "world" });
+	res.status(200).json({ hello: "world", CUSTOMENV: process.env.CUSTOMENV });
 }
 
 module.exports = helloController;
