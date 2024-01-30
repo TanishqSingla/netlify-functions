2bb9ca94e79a031c3b360ed9cc5ef81e56a1d324
TanishqSingla <tanishqsingla00@gmail.com>
2024-01-30 21:55:34 +0530

    fix: edit toml

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import { render } from 'preact';
 
 export function App() {
-	fetch("/api/healthcheck").then(data => data.json()).then(data => console.log(data));
+	fetch("/api/healthcheck").then(data => data.json()).then(data => console.log(data)).catch(e => console.log(e));
 	return (
 		<div>
 		hello world
