65333b47aafdf0dd0aac764483b42cb6bb148a0c
TanishqSingla <tanishqsingla00@gmail.com>
2024-01-30 21:41:00 +0530

    add cors

--- functions/api.js
+++ functions/api.js
@@ -4,6 +4,7 @@ import serverless from 'serverless-http';
 const app = express()
 const router = Router();
 
+app.use(cors());
 router.get("/healthcheck", (_req, res) => res.status(200).json({ status: "alive" }));
 app.use("/api/", router);
 
